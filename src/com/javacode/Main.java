8c62e3bfdd8a29e5330f42d3117eadb2fca66f0b
Hamid Shah Mohammadi <43700170+hamid-shahmohammadi@users.noreply.github.com>
2023-05-09 09:52:57 +0430

    2.create project

---
+++ src/com/javacode/Main.java
@@ -0,0 +1,10 @@
+package com.javacode;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("hihi");
+        System.out.println("ll ");
+        System.out.println(10 + 10);
+    }
+}
